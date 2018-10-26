6e07f5b888b3d4c416eaefadf3067615c3da4ac5
Karl Xu <karl.yogi.xu@gmail.com>
2018-10-26 13:17:04 +0800

    add console log to app.js access

--- app.js
+++ app.js
@@ -3,5 +3,6 @@ var http = require('http');
 //create a server object:
 http.createServer(function (req, res) {
   res.write('Hello World!'); //write a response to the client
+  console.log("Hello World!");
   res.end(); //end the response
 }).listen(8080); //the server object listens on port 8080
