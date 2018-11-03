7e3fa31f4eb15527041433d87e6f13ad0d7e7824
Karl Xu <karl.yogi.xu@gmail.com>
2018-11-03 19:02:26 +0800

    update stop to delete application

--- healthcheck.sh
+++ healthcheck.sh
@@ -4,7 +4,7 @@ set -ex
 URL=139.198.19.140 # put your URL here
 NODE="/root/.nvm/versions/node/v11.1.0/bin/node" # put the absolute path of NODE here
 PM2="/root/.nvm/versions/node/v11.1.0/bin/pm2" # put the absolute path of PM2 here
-PM2_CONFIG="/data/src/apps/pm2-default.config.js"
+PM2_CONFIG="/root/pm2-playground/pm2.yaml"
 
 
 for i in {0..1}
@@ -16,6 +16,7 @@ done
 
 if [[ ${RES[0]} != 200 && ${RES[0]} == ${RES[1]} ]]
 then
-    eval "$NODE $PM2 delete all && $NODE $PM2 reload $PM2_CONFIG"
+    eval "$NODE $PM2 delete all"
+    eval "$NODE $PM2 restart $PM2_CONFIG"
     if [ $? -ne 0 ] ; then echo "pm2 reload failed."; else echo "pm2 reload successful.";fi;
 fi
