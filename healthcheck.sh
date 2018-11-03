5a83afa74f01839de5261b69f603791ea0da9bff
Karl Xu <karl.yogi.xu@gmail.com>
2018-11-03 19:26:37 +0800

    update healthcheck.sh

--- healthcheck.sh
+++ healthcheck.sh
@@ -6,17 +6,19 @@ NODE="/root/.nvm/versions/node/v11.1.0/bin/node" # put the absolute path of NODE
 PM2="/root/.nvm/versions/node/v11.1.0/bin/pm2" # put the absolute path of PM2 here
 PM2_CONFIG="/root/pm2-playground/pm2.yaml"
 
+while true; do
+    sleep 5
+    for i in {0..1}
+    do
+        RES[$i]=`curl -s -I $URL | grep HTTP/1.1 | awk {'print $2'}`
+    done
 
-for i in {0..1}
-do
-    RES[$i]=`curl -s -I $URL | grep HTTP/1.1 | awk {'print $2'}`
+    # echo ${RES[0]} ${RES[1]}
+
+    if [[ ${RES[0]} != 200 && ${RES[0]} == ${RES[1]} ]]
+    then
+        eval "$NODE $PM2 delete $PM2_CONFIG "
+        eval "$NODE $PM2 reload $PM2_CONFIG"
+        if [ $? -ne 0 ] ; then echo "pm2 reload failed."; else echo "pm2 reload successful.";fi;
+    fi
 done
-
-# echo ${RES[0]} ${RES[1]}
-
-if [[ ${RES[0]} != 200 && ${RES[0]} == ${RES[1]} ]]
-then
-    eval "$NODE $PM2 delete $PM2_CONFIG "
-    eval "$NODE $PM2 reload $PM2_CONFIG"
-    if [ $? -ne 0 ] ; then echo "pm2 reload failed."; else echo "pm2 reload successful.";fi;
-fi
