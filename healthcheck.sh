cc125e5e628cf175d075c8badee26b57f7e9d0a1
Karl Xu <karl.yogi.xu@gmail.com>
2018-11-03 21:25:22 +0800

    add email notification feature

--- healthcheck.sh
+++ healthcheck.sh
@@ -5,6 +5,9 @@ URL=139.198.19.140 # put your URL here
 PM2_CONFIG="./pm2.yaml" # pm2 configuration file absolute path
 NODE="$(which node)" # put the absolute path of NODE here
 PM2="$(which pm2)" # put the absolute path of PM2 here
+ERROR_LOG="/var/log/cron"
+SUBJECT="PM2 Recovery $date"
+RECEIVER_EMAIL="karl.yogi.xu@gmail"
 
 while true; do
     sleep 5
@@ -20,5 +23,6 @@ while true; do
         eval "$NODE $PM2 delete $PM2_CONFIG "
         eval "$NODE $PM2 reload $PM2_CONFIG"
         if [ $? -ne 0 ] ; then echo "pm2 reload failed."; else echo "pm2 reload successful.";fi;
+        tail -100 ${ERROR_LOG} | mailx -s ${SUBJECT} ${RECEIVER_EMAIL}
     fi
 done
